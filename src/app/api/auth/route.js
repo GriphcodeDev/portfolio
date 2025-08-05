65c07ed4fca61307724de1a123073767f249bcef
Griphcode <148160412+GriphcodeDev@users.noreply.github.com>
2025-08-05 13:21:15 +0200

    Remove

--- src/app/api/auth/route.js
+++
@@ -1,9 +0,0 @@
-export async function POST(req) {
-  const { password } = await req.json();
-
-  if (password === process.env.CHAT_PASSWORD) {
-    return new Response(JSON.stringify({ success: true }), { status: 200 });
-  }
-
-  return new Response(JSON.stringify({ success: false }), { status: 401 });
-}
